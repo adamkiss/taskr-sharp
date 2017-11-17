534b1e85a56c72d1b4a5252418c08eb5692ca8cb
Adam Kiss <iam@adamkiss.com>
2017-11-17 21:37:07 +0100

    Switch tape to ava

---
+++ test/index.js
@@ -0,0 +1,31 @@
+const join = require('path').join
+const Taskr = require('taskr')
+const test = require('ava')
+
+const dir = join(__dirname, 'fixtures')
+const plugins = [require('@taskr/clear'), require('../')]
+
+const tmpDir = str => join(__dirname, str)
+const create = tasks => new Taskr({tasks, plugins})
+
+test('attach `sharp` to taskr and task', t => {
+	t.plan(3)
+
+	const taskr = create({
+		* default(task) {
+			t.true('sharp' in task)
+
+			const tmp = tmpDir('tmp1')
+			yield task.source(`${dir}/*.@(png|svg)`).target(tmp)
+
+			const arr = yield task.$.expand(`${tmp}/*.svg`)
+			t.is(arr.length, 1, 'copied two files to target tar')
+
+			yield task.clear(tmp) // Cleanup
+		}
+	})
+
+	t.true('sharp' in taskr.plugins)
+
+	return taskr.start()
+})
